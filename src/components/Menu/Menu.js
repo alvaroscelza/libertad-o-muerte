3cc954b7ad531df3ca60760cc9a294fc223f1d6c
Alvaro Scelza <alvaroscelza@gmail.com>
2022-08-26 22:37:28 -0300

    finished home

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -8,7 +8,7 @@ const Menu = () => {
             <h1 className={classes.h1}>Libertad o Muerte</h1>
             <h5 className={classes.h5}>Libertarismo en Uruguay</h5>
             <Link to='/'>
-                <img className={classes.img} src={profileImage} alt='Profile Image'/>
+                <img className={classes.img} src={profileImage} alt='Profile'/>
             </Link>
         </aside>
     );
--- src/components/Pages/HomePage.js
+++ src/components/Pages/HomePage.js
@@ -6,6 +6,7 @@ import freedomImage from '../../images/freedom.png';
 import privatePropertyImage from '../../images/private-property.png';
 import ExternalLink from "../UI/ExternalLink";
 import HumanRightBlock from "./HumanRightBlock";
+import politicalCompassImage from '../../images/political-compass.webp';
 
 const HomePage = () => {
     useEffect(() => {
@@ -17,7 +18,7 @@ const HomePage = () => {
     return (
         <>
             <h1 className={generalClasses.h1}>¿Qué es el Libertarismo?</h1>
-            <p className={generalClasses.p}>
+            <p className={homePageClasses.justifyText}>
                 Es una ideología política que nace
                 del <ExternalLink link={NAPLink} text="Principio de No Agresión (NAP)"/>, que prima la libertad del
                 individuo por sobre todo lo demás y se construye alrededor de tres derechos fundamentales,
@@ -38,6 +39,34 @@ const HomePage = () => {
                 </HumanRightBlock>
             </div>
             <h2 className={generalClasses.h2}>El Libertarismo, ¿Es de derechas o de izquierdas?</h2>
+            <p className={homePageClasses.justifyText}>
+                Primero debemos entender que el clásico espectro político Izquierda-Derecha al que las personas estamos
+                tan acostumbradas está incompleto. Apenas incluye algunas ideologías políticas de la enorme cantidad
+                existente, y limitarse a dicho espectro es uno de los principales errores de los latinoamericanos, que
+                sabemos muy poco sobre política. Esto explica muchos de nuestros problemas: hace más de 200 años que
+                venimos probando alternativas repetidas dentro de una visión muy limitada de las verdaderas
+                posibilidades. <br/>
+                 <br/>
+                En 1950 Hans Eysenck percibió que pese a estar en los extremos opuestos, el Nacional Comunismo
+                (aplicado en la URRS) y el Fascismo tenían un sorprendente número de características similares. Otros
+                pensadores de la época detectaron problemas similares y con el tiempo, nuevos modelos políticos fueron
+                surgiendo. <br/>
+                 <br/>
+                Hoy en día el más utilizado es el compás político de dos ejes, o "The Political Compass". <br/>
+                El eje horizontal coincide con el del espectro clásico: izquierda-derecha. Donde la izquierda defiende
+                derechos sociales, y la derecha defiende derechos económicos.
+                El nuevo eje, el vertical, define dos nuevos opuestos, totalitarismo versus libertad:
+            </p>
+            <div className={homePageClasses.politicalCompassImageContainer}>
+                <img src={politicalCompassImage} alt='Political Compass' width="500" height="500"/>
+            </div>
+            <p className={homePageClasses.centerText}>
+                Como puedes ver, el Nacional Comunismo y el Fascismo, pese a corresponder a izquierda y derecha
+                respectivamente, tienen mucho en común por ser ambos totalitarios. <br/>
+                <br/>
+                El Libertarismo es la oposición perfecta a ambos: prima la libertad, defendiendo los derechos sociales.
+                Y en el caso del libertarismo de derecha, también defiende tus derechos económicos.
+            </p>
         </>
     );
 }
